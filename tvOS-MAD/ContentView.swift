2d214ffd9f3362f4e7f1e7bd86136c12b9d8705c
starosta64 <kro00@mail.ru>
2021-06-06 13:04:46 +0300

    Initial Commit

---
+++ tvOS-MAD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tvOS-MAD
+//
+//  Created by user on 06.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tvOS-MAD/tvOS_MADApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvOS_MADApp.swift
+//  tvOS-MAD
+//
+//  Created by user on 06.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct tvOS_MADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
